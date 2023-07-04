8ca5b5bd3756a2df57e98428fc59bea7823dd5a0
Mathias Rav <m@git.strova.dk>
2023-07-04 22:36:46 +0100

    Refactor and add quadratic time algorithm

--- single_from_qhull.py
+++ single_from_qhull.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 
 from scipy.sparse import coo_array, csr_matrix
@@ -8,6 +10,16 @@ import scipy.cluster.hierarchy
 
 
 def single_linkage(points):
+    # Compute Euclidean MST using Delaunay triangulation, then run Union-Find.
+    return single_linkage_from_mst(euclidean_mst_scipy(points))
+
+
+def single_linkage_quadratic_time(points):
+    # Compute Euclidean MST using a quadratic algorithm, then run Union-Find.
+    return single_linkage_from_mst(euclidean_mst_quadratic(points))
+
+
+def euclidean_mst_scipy(points):
     # Run Delaunay triangulation to obtain a suitable input for Euclidean MST.
     tri = Delaunay(points)
     u, v = np.transpose(
@@ -23,14 +35,59 @@ def single_linkage(points):
     # Obtain relevant Euclidean distances for Euclidean MST.
     data = ((points[u] - points[v]) ** 2).sum(axis=1) ** 0.5
 
-    # Run Euclidean MST.
+    # Run MST on Euclidean-weighted sparse graph.
     X = coo_array((data, (u, v)), shape=(len(points), len(points)))
-    Tcsr = coo_array(minimum_spanning_tree(csr_matrix(X)))
+    return coo_array(minimum_spanning_tree(csr_matrix(X)))
 
-    # Run Union-Find to obtain single-linkage clustering.
+
+def euclidean_mst_quadratic(points):
+    # Prim's algorithm to compute a minimum spanning tree:
+    # Initialize the MST with a single arbitrary input point.
+    # Repeat the following N-1 times:
+    # Identify the closest point not already added to the MST,
+    # and add it to the MST.
+    # Usually this would be implemented with an efficient priority queue,
+    # leading to an N log N runtime.
+    # But this is Python, so our priority queue is a simple unsorted array,
+    # leading to quadratic runtime.
+    data = []
+    u = []
+    v = []
+
+    # non-nan dists[i] is the Euclidean distance between i and closest[i]
+    # if dists[i] is nan, i has already been added to the MST
+    dists = ((points[0:1] - points) ** 2).sum(axis=1) ** 0.5
+    dists[0] = np.nan
+    closest = np.array([0] * len(points), dtype=np.intp)
+    for _ in range(len(points)):
+        # Add the closest not-yet-added point into the MST
+        try:
+            i = np.nanargmin(dists)
+        except ValueError:
+            # All points added
+            break
+        # Add to MST
+        data.append(dists[i])
+        u.append(i)
+        v.append(closest[i])
+        # Update dists and closest with distances to i
+        newdists = ((points[i : i + 1] - points) ** 2).sum(axis=1) ** 0.5
+        newclosest = newdists < dists
+        closest[newclosest] = i
+        np.minimum(dists, newdists, out=dists)
+        # Mark i as visited
+        dists[i] = np.nan
+    # After n-1 iterations, all should be visited, so all of dists should be nan
+    assert np.all(np.isnan(dists))
+    return coo_array((data, (u, v)), shape=(len(points), len(points)))
+
+
+def single_linkage_from_mst(Tcsr):
+    # Run Union-Find on MST to obtain single-linkage clustering.
     Z = []
-    parent = list(range(len(points)))
-    subtree_size = [1] * len(points)
+    n = Tcsr.shape[0]
+    parent = list(range(n))
+    subtree_size = [1] * n
 
     def representative(i):
         while parent[i] != i:
@@ -72,8 +129,25 @@ def main() -> None:
     assert np.array_equal(a, b)
 
     np.random.seed(seed)
-    pts = np.random.random((64000, 2))
+    pts = np.random.random((10000, 2))
+    t1 = time.time()
     print(single_linkage(pts)[-1])
+    t2 = time.time()
+    print(f"Time to run N log N time algorithm on {pts.shape[0]} {pts.shape[1]}-d points: {t2 - t1} s")
+
+    np.random.seed(seed)
+    pts = np.random.random((10000, 2))
+    t1 = time.time()
+    print(single_linkage_quadratic_time(pts)[-1])
+    t2 = time.time()
+    print(f"Time to run N² time algorithm on {pts.shape[0]} {pts.shape[1]}-d points: {t2 - t1} s")
+
+    np.random.seed(seed)
+    pts = np.random.random((100000, 2))
+    t1 = time.time()
+    print(single_linkage(pts)[-1])
+    t2 = time.time()
+    print(f"Time to run N log N time algorithm on {pts.shape[0]} {pts.shape[1]}-d points: {t2 - t1} s")
 
 
 if __name__ == "__main__":
